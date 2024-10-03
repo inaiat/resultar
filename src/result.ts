f10f12ab15727c0506b17c2b90d142d069951fe2
Inaiat Moraes <inaiat@gmail.com>
2024-10-02 23:30:20 -0300

    feat: safeTry can handle ResultAsybc

--- src/result.ts
+++ src/result.ts
@@ -1,7 +1,7 @@
 import type { ErrorConfig } from './error.js'
 import { createResultarError } from './error.js'
 import { errAsync, ResultAsync } from './result-async.js'
-import type { CombineResults, CombineResultsWithAllErrorsArray, InferErrTypes, InferOkTypes } from './utils.js'
+import type { ExtractErrTypes, ExtractOkTypes, InferErrTypes, InferOkTypes } from './utils.js'
 import { combineResultList, combineResultListWithAllErrors } from './utils.js'
 
 class Ok<T> {
@@ -325,7 +325,7 @@ export class Result<T, E> implements Resultable<T, E> {
    * @param ok
    * @param err
    */
-  match<X>(fnOk: (t: T) => X, fnErr: (e: E) => X): X {
+  match<A, B = A>(fnOk: (t: T) => A, fnErr: (e: E) => B): A | B {
     if (this.state.ok) {
       return fnOk(this.value)
     }
@@ -498,6 +498,16 @@ export const { ok, err, fromThrowable, unit } = Result
  * @returns The first occurence of either an yielded Err or a returned Result.
  */
 export function safeTry<T, E>(body: () => Generator<Result<never, E>, Result<T, E>>): Result<T, E>
+export function safeTry<
+  YieldErr extends Result<never, unknown>,
+  GeneratorReturnResult extends Result<unknown, unknown>,
+>(
+  body: () => Generator<YieldErr, GeneratorReturnResult>,
+): Result<
+  InferOkTypes<GeneratorReturnResult>,
+  InferErrTypes<YieldErr> | InferErrTypes<GeneratorReturnResult>
+>
+
 /**
  * Evaluates the given generator to a Result returned or an Err yielded from it,
  * whichever comes first.
@@ -510,21 +520,209 @@ export function safeTry<T, E>(body: () => Generator<Result<never, E>, Result<T, 
  * `yield* resultAsync.safeUnwrap()` work as Rust's `result?` expression.
  * @returns The first occurence of either an yielded Err or a returned Result.
  */
-// NOTE:
-// Since body is potentially throwable because `await` can be used in it,
-// Promise<Result<T, E>>, not ResultAsync<T, E>, is used as the return type.
 export function safeTry<T, E>(
   body: () => AsyncGenerator<Result<never, E>, Result<T, E>>,
-): Promise<Result<T, E>>
+): ResultAsync<T, E>
+export function safeTry<
+  YieldErr extends Result<never, unknown>,
+  GeneratorReturnResult extends Result<unknown, unknown>,
+>(
+  body: () => AsyncGenerator<YieldErr, GeneratorReturnResult>,
+): ResultAsync<
+  InferOkTypes<GeneratorReturnResult>,
+  InferErrTypes<YieldErr> | InferErrTypes<GeneratorReturnResult>
+>
 export function safeTry<T, E>(
   body:
     | (() => Generator<Result<never, E>, Result<T, E>>)
     | (() => AsyncGenerator<Result<never, E>, Result<T, E>>),
-): Result<T, E> | Promise<Result<T, E>> {
+): Result<T, E> | ResultAsync<T, E> {
   const n = body().next()
   if (n instanceof Promise) {
-    return n.then(r => r.value)
+    return new ResultAsync(n.then((r) => r.value))
   }
 
   return n.value
 }
+
+// #region Combine - Types
+
+// This is a helper type to prevent infinite recursion in typing rules.
+//
+// Use this with your `depth` variable in your types.
+type Prev = [
+  never,
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+  30,
+  31,
+  32,
+  33,
+  34,
+  35,
+  36,
+  37,
+  38,
+  39,
+  40,
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48,
+  49,
+  ...Array<0>,
+]
+
+// Collects the results array into separate tuple array.
+//
+// T         - The array of the results
+// Collected - The collected tuples.
+// Depth     - The maximum depth.
+type CollectResults<T, Collected extends unknown[] = [], Depth extends number = 50> = [
+  Depth,
+] extends [never] ? []
+  : T extends [infer H, ...infer Rest]
+  // And test whether the head of the list is a result
+    ? H extends Result<infer L, infer R>
+      // Continue collecting...
+      ? CollectResults<
+        // the rest of the elements
+        Rest,
+        // The collected
+        [...Collected, [L, R]],
+        // and one less of the current depth
+        Prev[Depth]
+      >
+    : never // Impossible
+  : Collected
+
+// Transposes an array
+//
+// A          - The array source
+// Transposed - The collected transposed array
+// Depth      - The maximum depth.
+export type Transpose<
+  A,
+  Transposed extends unknown[][] = [],
+  Depth extends number = 10,
+> = A extends [infer T, ...infer Rest]
+  ? T extends [infer L, infer R]
+    ? Transposed extends [infer PL, infer PR]
+      ? PL extends unknown[] ? PR extends unknown[] ? Transpose<Rest, [[...PL, L], [...PR, R]], Prev[Depth]>
+        : never
+      : never
+    : Transpose<Rest, [[L], [R]], Prev[Depth]>
+  : Transposed
+  : Transposed
+
+// Combines the both sides of the array of the results into a tuple of the
+// union of the ok types and the union of the err types.
+//
+// T     - The array of the results
+// Depth - The maximum depth.
+export type Combine<T, Depth extends number = 5> = Transpose<CollectResults<T>, [], Depth> extends [
+  infer L,
+  infer R,
+] ? [UnknownMembersToNever<L>, UnknownMembersToNever<R>]
+  : Transpose<CollectResults<T>, [], Depth> extends [] ? [[], []]
+  : never
+
+// Deduplicates the result, as the result type is a union of Err and Ok types.
+export type Dedup<T> = T extends Result<infer RL, infer RR> ? Result<RL, RR>
+  : T
+
+// Given a union, this gives the array of the union members.
+export type MemberListOf<T> = (
+  (T extends unknown ? (t: T) => T : never) extends infer U
+    ? (U extends unknown ? (u: U) => unknown : never) extends (v: infer V) => unknown ? V
+    : never
+    : never
+) extends (_: unknown) => infer W ? [...MemberListOf<Exclude<T, W>>, W]
+  : []
+
+// Converts an empty array to never.
+//
+// The second type parameter here will affect how to behave to `never[]`s.
+// If a precise type is required, pass `1` here so that it will resolve
+// a literal array such as `[ never, never ]`. Otherwise, set `0` or the default
+// type value will cause this to resolve the arrays containing only `never`
+// items as `never` only.
+export type EmptyArrayToNever<T, NeverArrayToNever extends number = 0> = T extends [] ? never
+  : NeverArrayToNever extends 1 ? T extends [never, ...infer Rest] ? [EmptyArrayToNever<Rest>] extends [never] ? never
+      : T
+    : T
+  : T
+
+// Converts the `unknown` items of an array to `never`s.
+type UnknownMembersToNever<T> = T extends [infer H, ...infer R]
+  ? [[unknown] extends [H] ? never : H, ...UnknownMembersToNever<R>]
+  : T
+
+// Gets the member type of the array or never.
+export type MembersToUnion<T> = T extends unknown[] ? T[number] : never
+
+// Checks if the given type is a literal array.
+export type IsLiteralArray<T> = T extends { length: infer L } ? L extends number ? number extends L ? 0
+    : 1
+  : 0
+  : 0
+
+// Traverses an array of results and returns a single result containing
+// the oks and errs union-ed/combined.
+type Traverse<T, Depth extends number = 5> = Combine<T, Depth> extends [infer Oks, infer Errs]
+  ? Result<EmptyArrayToNever<Oks, 1>, MembersToUnion<Errs>>
+  : never
+
+// Traverses an array of results and returns a single result containing
+// the oks combined and the array of errors combined.
+type TraverseWithAllErrors<T, Depth extends number = 5> = Traverse<T, Depth> extends Result<
+  infer Oks,
+  infer Errs
+> ? Result<Oks, Errs[]>
+  : never
+
+// Combines the array of results into one result.
+export type CombineResults<
+  T extends ReadonlyArray<Result<unknown, unknown>>,
+> = IsLiteralArray<T> extends 1 ? Traverse<T>
+  : Result<ExtractOkTypes<T>, ExtractErrTypes<T>[number]>
+
+// Combines the array of results into one result with all errors.
+export type CombineResultsWithAllErrorsArray<
+  T extends ReadonlyArray<Result<unknown, unknown>>,
+> = IsLiteralArray<T> extends 1 ? TraverseWithAllErrors<T>
+  : Result<ExtractOkTypes<T>, Array<ExtractErrTypes<T>[number]>>
+
+// #endregion
